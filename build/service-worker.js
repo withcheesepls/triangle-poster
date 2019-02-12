b7b5142e8e815ba2a99d5dd2e1565ee31e901713
withcheesepls <josue.rojas.rojas@gmail.com>
2019-02-12 08:31:01 -0500

    refactor some code

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89e808732eabe4c38d84b8e869d6743d.js"
+  "/triangle-poster/precache-manifest.b988688fb34660233d08c506150e5a7f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/triangle-poster/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/Components/Triangle.js
+++ src/Components/Triangle.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import '../Styles/Triangle.css';
 
-const SQRROOT3 = Math.sqrt(3);
-
 export default class Triangle extends React.Component {
-  // return the border size for triangle
   getTriangleStyle(width, firstColor, secondColor, zindex){
     const fColor = `rgb(${firstColor[0]}, ${firstColor[1]}, ${firstColor[2]})`;
     const sColor = `rgb(${secondColor[0]}, ${secondColor[1]}, ${secondColor[2]})`;
@@ -15,15 +12,13 @@ export default class Triangle extends React.Component {
       zIndex: zindex
     }
   }
+
   createTriangles(amount, width, firstColor, secondColor){
     let triangles = [];
-    let triangleDiff = 32;
-    let colorDif = [];
-    colorDif.push(Math.round((secondColor[0] > firstColor[0] ? -(secondColor[0] - firstColor[0]) : firstColor[0] - secondColor[0])/amount));
-    colorDif.push(Math.round((secondColor[1] > firstColor[1] ? -(secondColor[1] - firstColor[1]) : firstColor[1] - secondColor[1])/amount));
-    colorDif.push(Math.round((secondColor[0] > firstColor[2] ? -(secondColor[2] - firstColor[2]) : firstColor[2] - secondColor[2])/amount));
+    let triangleDiff = 32; // this should make sure the width triangle is not less than 0
+    let colorDif = firstColor.map((e, i)=> (Math.round((secondColor[i] > firstColor[i] ? -(secondColor[i] - firstColor[i]) : firstColor[i] - secondColor[i])/amount)));
     let fColor = firstColor.slice();
-    let sColor = [fColor[0]-colorDif[0], fColor[1]-colorDif[1], fColor[2]-colorDif[2]];
+    let sColor = fColor.map((e, i) => e - colorDif[i]);
     let zIndex = 1;
     for(let i = 0; i < amount; i++){
       let style = {};
@@ -39,30 +34,23 @@ export default class Triangle extends React.Component {
       )
       if(i !== 0 || i !== amount-1){
         fColor = sColor.slice();
-        sColor = [fColor[0]-colorDif[0], fColor[1]-colorDif[1], fColor[2]-colorDif[2]];
+        sColor = fColor.map((e, i) => e - colorDif[i]);
 
       }
       width-=triangleDiff;
     }
     return triangles
   }
-  getTriangleWidth(width){
-    const leftright = width/2;
-    const height = width*SQRROOT3/2;
-    return `0 ${leftright}px ${height}px ${leftright}px`
-  }
+
   render(){
-    const width = this.props.width;
-    const firstColor = this.props.firstColor;
-    const secondColor = this.props.secondColor;
+    const {width, numTriangles, firstColor, secondColor} = this.props;
     const styleTriangleWrapper = {
       width: `${width}px`,
       height: `${width}px`
     }
-
     return(
       <div style={styleTriangleWrapper} className='triangle-wrapper'>
-        {this.createTriangles(this.props.numTriangles, width, firstColor, secondColor)}
+        {this.createTriangles(numTriangles, width, firstColor, secondColor)}
       </div>
     )
   }
