c442f504f6b03e4af6874078e17aef2c9b36a2c1
withcheesepls <josue.rojas.rojas@gmail.com>
2019-02-12 00:03:39 -0500

    added gh-pages

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # triangle-poster
 A poster inspired by Paul Methot
+
+Inspired by https://dribbble.com/shots/4864465-PosterSeries-BasicBlends-Jungle
+
+If you like the design make sure to check out the designer's page.
+
+A little over done with react but i wanted the triangles to be dynamic and be able to change the color easier than hardcoding with css.
---
+++ build/precache-manifest.89e808732eabe4c38d84b8e869d6743d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c9195b98bb00de45c59e",
+    "url": "/static/js/main.c9195b98.chunk.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "c9195b98bb00de45c59e",
+    "url": "/static/css/main.15ea11c4.chunk.css"
+  },
+  {
+    "revision": "c42d2d2245be10a58248638fb8f0abf4",
+    "url": "/index.html"
+  }
+];
